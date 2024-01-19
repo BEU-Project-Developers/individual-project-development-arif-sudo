8f154c98fb8fb84c250392a53ff931df9a44dfbe
Arif <arif520gg@gmail.com>
2024-01-19 15:19:37 +0400

    Connection string created

---
+++ Dental_Clinic_Management/Connection/ConnectionString.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dental_Clinic_Management.Connection
+{
+    public class ConnectionString
+    {
+        public SqlConnection GetCon() { 
+            SqlConnection connection = new SqlConnection();
+            connection.ConnectionString = @"Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\USER\\Desktop\\individual-project-development-arif-sudo\\Dental_Clinic_Management\\DentalClinicDb.mdf;Integrated Security=True;Connect Timeout=30";
+            return connection; 
+        }
+    }
+}
